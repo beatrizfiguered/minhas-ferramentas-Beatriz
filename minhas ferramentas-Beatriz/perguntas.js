2d9f16f701a4248da5b56ed66bad09268b169197
beatrizfiguered <162448447+beatrizfiguered@users.noreply.github.com>
2024-11-21 09:07:03 -0300

    Add files via upload

---
+++ minhas ferramentas-Beatriz/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é github?',
+    'O github é uma repositorio de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz escola em Inglês?',
+    'escola em ingles é school (ISKUL)'
+)
